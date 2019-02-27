1d23ca57de51e16e1aa079fdecb79bff94cac6fc
Gianni Salinetti <gbsalinetti@extraordy.com>
2019-02-28 00:08:01 +0100

    Update env init

--- katacodacomextraordygolang-demo/env-init.sh
+++ katacodacomextraordygolang-demo/env-init.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+GO_LINUX_URL="https://dl.google.com/go/go1.12.linux-amd64.tar.gz"
+GOROOT=/goroot
+GO_ARCHIVE="go1.12.linux-amd64.tar.gz"
+
+curl -o /tmp/${GO_ARCHIVE} $GO_LINUX_URL
+
+tar zxvf /tmp/${GO_ARCHIVE} -C /tmp
+
+rm -rf /goroot/* && cp -a /tmp/go/* /goroot
+
 echo "background stuff" > /tmp/background.txt
 
