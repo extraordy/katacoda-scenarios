12146842466298d0af00c0458e4875819d597551
Gianni Salinetti <gbsalinetti@extraordy.com>
2019-02-28 00:20:54 +0100

    Update terminal message and icon

--- katacodacomextraordygolang-demo/env-init.sh
+++ katacodacomextraordygolang-demo/env-init.sh
@@ -4,17 +4,21 @@ GO_LINUX_URL="https://dl.google.com/go/go1.12.linux-amd64.tar.gz"
 GOROOT=/goroot
 GO_ARCHIVE="go1.12.linux-amd64.tar.gz"
 
+# Download Go binary archive
 curl -o /tmp/${GO_ARCHIVE} $GO_LINUX_URL
 if [ $? -ne 0 ]; then
     echo "Error: unable to download Go archive" >> /tmp/env-init.log
     exit 1
 fi
 
+# Unpack the archive
 tar zxvf /tmp/${GO_ARCHIVE} -C /tmp && rm -rf /tmp/${GO_ARCHIVE}
 
+# Remove oldest Go version and install
 rm -rf /goroot/* && cp -a /tmp/go/* /goroot
 if [ $? -ne 0 ]; then
     echo "Error: unable to copy files to goroot" >> /tmp/env-init.log
     exit 1
 fi
 
+exit 0
